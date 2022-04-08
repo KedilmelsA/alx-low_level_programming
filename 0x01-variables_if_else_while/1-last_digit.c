fa7fb146f564563cd213564633deeb06e6abeaf8
kedilmelsA <kedilmels@hu.edu.et>
2022-04-08 17:59:01 +0300

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,50 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Prints if number is positive, zero or negative
  * Return: Always (Success)
  */
 int main(void)
 {
-  int n, l;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  l = n % 10;
-  if (l > 5)
-  {
-    printf("Last digit of %d is %d and is greater than 5\n", n, l);
-  }
-  else if (l == 0)
-  {
-    printf("Last digit of %d is %d and is 0\n", n, l);
-  }
-  else
-  {
-    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-  }
-  return (0);
-}
- 19  
-0x01-variables_if_else_while/2-print_alphabet.c
-@@ -0,0 +1,19 @@
-#include <stdio.h>
+	int n, lastd;
 
-/
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-  char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-  int i;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
 
-  for (i = 0; i < 26; i++)
-  {
-    putchar(alp[i]);
-  }
-  putchar('\n');
-  return (0);
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+
+	return (0);
 }
