cfa39020a0bebf6728232dbf963fda6594fb3ab3
kedilmelsA <kedilmels@hu.edu.et>
2022-04-12 20:36:40 +0300

    Task 7 commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _abs - value absolute
+ *  print_last_digit - last digit
+ *
  *@n: The int to print
  * Return: Always 0.
  */
-int _abs(int n)
+
+int print_last_digit(int n)
+
 {
+int last_digit;
 
 if (n < 0)
 {
-return (n * (-1));
-}
-
-else if (n == 0)
-{
-return (0);
+last_digit = (-1 * (n % 10));
+_putchar (last_digit + '0');
+return (last_digit);
 }
 
 else
 {
-return (n);
+last_digit = (n % 10);
+_putchar (last_digit + '0');
+return (last_digit);
 }
 
 }
