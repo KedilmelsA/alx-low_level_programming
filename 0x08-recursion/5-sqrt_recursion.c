140eecf8e2a82c312b5dc32ef6d64c2669225b50
kedilmelsA <kedilmels@hu.edu.et>
2022-04-27 18:29:23 +0300

    Task 5 Commit Message

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/
+/*
  * sqrt2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
@@ -16,7 +16,7 @@ int sqrt2(int a, int b)
     return (-1);
   return (sqrt2(a, b + 1));
 }
-/
+/*
  * _sqrt_recursion - returns the natural square root of n
  * @n: Number Integer
  *
