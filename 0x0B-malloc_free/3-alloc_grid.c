1166a6c08a85ae99e6c58e50bb6a9a9fdf70a0b9
KedilmelsA <kedilmels@hu.edu.et>
2022-05-03 17:59:54 +0300

    Write a function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
- * Return: pointer to 2 dim. array
+ * alloc_grid - create a matrix using malloc
+ * @width: width of the matrix
+ * @height: height of the matrix
+ *
+ * Return: return a pointer of pointer or null
  */
 int **alloc_grid(int width, int height)
 {
-	int **iarray;
-	int i, n;
+	int i, j, a, b;
+	int **p;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	iarray = malloc(sizeof(int) * height);
-
-	if (iarray == NULL)
 	{
-		free(iarray);
 		return (NULL);
 	}
-
-	for (i = 0; i < height; i++)
+	else
 	{
-		iarray[i] = malloc(sizeof(int) * width);
-		if (iarray == NULL)
+		p = (int **) malloc(height * sizeof(int *));
+		/* we have to make a malloc per pointer*/
+		if (!p)
 		{
-			free(iarray);
+			free(p);
 			return (NULL);
 		}
-
-		for (n = 0; n < width; n++)
+		for (i = 0; i < height; i++)
 		{
-			iarray[i][n] = 0;
+			p[i] = (int *) malloc(width * sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
 		}
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				p[a][b] = 0;
+			}
+		}
+		return (p);
 	}
-	return (iarray);
 }
