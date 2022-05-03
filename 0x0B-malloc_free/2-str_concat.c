2b0ff775fcd47323dc94af764657a7c1b43cf6ee
KedilmelsA <kedilmels@hu.edu.et>
2022-05-03 17:56:24 +0300

    Write a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
- */
+  *str_concat - concatenates 2 strings.
+  * a NULL string is treated as an empty string.
+  *@s1: pointer to string.
+  *@s2: pointer to string.
+  *
+  *Return: pointer to newly allocated memory which
+  *has s1, s2 and null byte.
+  *NULL on failure.
+  */
 char *str_concat(char *s1, char *s2)
 {
-	int end1, end2, i = 0;
-	char *array;
+	unsigned int len1, len2, size, i, j;
+	char *nstr;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	for (end1 = 0; end1 <= *s1; end1++)
-	{
-	}
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
 
-	for (end2 = 0; end2 <= *s2; end2++)
-	{
-	}
+	size = len1 + len2;
 
-	array = malloc(sizeof(char) * (end1 + end2 + 1));
-
-	if (array == NULL)
+	nstr = malloc((sizeof(char) * size) + 1);
+	/*check if malloc was successful */
+	if (nstr == NULL)
 		return (NULL);
 
-	while (*s1)
+	i = 0;
+	while (i < len1)
 	{
-		array[i] = *s1;
+		nstr[i] = s1[i];
 		i++;
-		s1++;
 	}
-
-	while (*s2)
+	j = 0;
+	while (i <= size)
 	{
-		array[i] = *s2;
+		nstr[i] = s2[j];
 		i++;
-		s2++;
+		j++;
 	}
-	return (array);
+	return (nstr);
 }
