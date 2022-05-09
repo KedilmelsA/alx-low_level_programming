2c335ca19ac90de9ffef46b36e57b6b0b77ff213
KedilmelsA <kedilmels@hu.edu.et>
2022-05-09 21:44:52 +0300

    MY zero task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
-ifndef STRTYP
-#define STRTYP
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a new type struct dog.
+ */
 
 /**
-  *struct dog - structure of a dog.
-  *@name: name of dog.
-  *@age: age of dog.
-  *@owner: owner of dog.
-  */
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
@@ -14,6 +20,9 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -21,4 +30,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*STRTYP*/
+#endif
